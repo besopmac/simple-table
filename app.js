aaf14abbdf726f738cd81ef00303e8232b91b30c
Bruno Campos <brunosopmac@gmail.com>
2019-01-06 19:16:11 -0200

    Servindo arquivos

---
+++ app.js
@@ -0,0 +1,8 @@
+const app = require('express')();
+const path = require('path');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen('3000');
--- js/app.js
+++
@@ -1,9 +0,0 @@
-const app = require('express')();
-
-app.listen('3000', (req, res) => {
-    console.log('Servidor OK');
-});
-
-app.get('/', (req, res) => {
-    res.send(req.query);
-});
